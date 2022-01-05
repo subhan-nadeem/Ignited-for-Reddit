a12ec85845c2d386d6e071cc4e4d1657b354a958
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-06 01:05:10 +0800

    Delete app directory

--- app/src/main/java/com/subhan_nadeem/ignite/subreddit_selection/OnSubredditSavedListener.java
+++
@@ -1,9 +0,0 @@
-package com.subhan_nadeem.ignite.subreddit_selection;
-
-/**
- * Created by Subhan Nadeem on 2017-10-02.
- */
-
-public interface OnSubredditSavedListener {
-    public void onSubredditSaved(boolean showSaved, String subredditName);
-}
